6118cb7934378395bead933fae4d3fb8b9757cb9
BADAKAA <81486945+BADAKAA@users.noreply.github.com>
2026-06-27 00:58:12 +0200

    add core script

---
+++ code/script.R
@@ -0,0 +1,8 @@
+# ============================================================================ #
+# ---------------------- INEQUALITY AND REDISTRIBUTION ----------------------- #
+# ============================================================================ #
+
+source("code/setup.R")
+if (!file.exists(PATHS$soep)) source("code/transform_soep.R")
+source("code/plot_income.R")
+source("code/did_triple.R")
