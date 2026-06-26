37523f584132957a9425ba6396782eda2e9b8edf
BADAKAA <81486945+BADAKAA@users.noreply.github.com>
2026-06-27 00:51:39 +0200

    add setup script

---
+++ code/setup.R
@@ -0,0 +1,15 @@
+rm(list = ls())
+palette("Okabe-Ito")
+
+required_pkgs <- c("tidyverse", "readr", "dplyr", "haven", "fixest", "modelsummary", "stargazer", "tinytable") # nolint
+missing_pkgs <- required_pkgs[!(required_pkgs %in% installed.packages()[, "Package"])] # nolint
+if (length(missing_pkgs) > 0) {
+  message("Installing missing R packages: ", paste(missing_pkgs, collapse = ", ")) # nolint
+  install.packages(missing_pkgs, repos = "https://cloud.r-project.org")
+}
+
+source("code/utils.R")
+for (dir in DIRS) {
+  if (dir.exists(dir)) next
+  dir.create(dir, recursive = TRUE, showWarnings = FALSE)
+}
